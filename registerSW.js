696fcad8e0ba19016a1357ba9776d2db3c08e2fe
Bubblez360 <Bubblez360@users.noreply.github.com>
2026-06-14 11:30:58 +0000

    deploy: 058fbf9d6682eaebee4302514b8fcf7b1c5528dd

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SimplePosSystem/sw.js', { scope: '/SimplePosSystem/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
